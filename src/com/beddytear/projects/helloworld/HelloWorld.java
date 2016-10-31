daca382abd7831d95426993046709f9523d5665f
Max Knechten <max.knechten@gmail.com>
2016-10-31 10:01:50 +0100

    Committing "awesome" stuff

--- src/com/beddytear/projects/helloworld/HelloWorld.java
+++ src/com/beddytear/projects/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package com.beddytear.projects.helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println("Hello awesome world!");
 	}
 
 }
